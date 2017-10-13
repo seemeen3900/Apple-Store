b11895c3ffca5b4ca5bd1888e85dec367e33aaf0
seemeen3900 <seemeen3900@gmail.com>
2017-10-12 20:27:05 -0400

    First Commit

---
+++ src/main/java/com/Apple/app/App.java
@@ -0,0 +1,13 @@
+package com.Apple.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
